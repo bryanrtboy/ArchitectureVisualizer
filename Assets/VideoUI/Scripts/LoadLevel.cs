029e16f2e3f68d5b2ba038f758382f8cf40a5ef7
cudigitaldesign <ucdenverdesign@gmail.com>
2017-02-27 10:51:07 -0700

    Added Start Script

---
+++ Assets/VideoUI/Scripts/LoadLevel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+
+public void LoadTheLevel(int level)
+    {
+        SceneManager.LoadScene(1);
+    }
+}
